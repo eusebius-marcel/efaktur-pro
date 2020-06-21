a0f21ca4f20835698a2adec671dbb4e9309c41b9
Eusebius Marcel <eusebius.em@gmail.com>
2020-06-21 17:49:04 +0700

    Setup react router and set rails route to send all non-API request to static controller

---
+++ app/javascript/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Hello from './Hello'
+
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' render={() => ('Home!')}/>
+          <Route path='/hello' render={() => <Hello />}/>
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax (!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ("/") path.
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+  # Forward root to StaticController#index
   root 'static#index'
 
   namespace :api do
